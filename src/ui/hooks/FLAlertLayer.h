057d983e6456966bc5101ddac50b5eb742f08096
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-11-21 04:26:46 -0500

    my bad, gd::string moment

--- src/ui/hooks/FLAlertLayer.h
+++ src/ui/hooks/FLAlertLayer.h
@@ -3,7 +3,7 @@
 
 class $modify(MyFLAlertLayer, FLAlertLayer){
 
-    bool init(FLAlertLayerProtocol* p0, char const* p1, std::string p2, char const* p3, char const* p4, float p5, bool p6, float p7, float p8){
+    bool init(FLAlertLayerProtocol* p0, char const* p1, gd::string p2, char const* p3, char const* p4, float p5, bool p6, float p7, float p8){
 	
         if(!FLAlertLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8)){
             return false;
